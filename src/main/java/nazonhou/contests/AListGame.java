0e1b57ad4a1c6ee018f0afc35d71f0bae5178c15
AZONHOU Nelson <cmdtazonhou@gmail.com>
2023-10-27 17:41:41 +0100

    AListGame

--- src/main/java/nazonhou/contests/AListGame.java
+++ src/main/java/nazonhou/contests/AListGame.java
@@ -1,49 +1,27 @@
 package nazonhou.contests;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class AListGame {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
-    int maximumValueOfX = 10_000_000;
-
-    List<Integer> primes = new ArrayList<Integer>(maximumValueOfX - 1);
-
-    for (int iteration = 2; iteration <= maximumValueOfX / 2; iteration++) {
-      // System.out.println(iteration);
-      primes.add(iteration);
-    }
-
-    int iteration = 0;
-
-    while (iteration < primes.size()) {
-      int prime = primes.get(iteration);
-
-      for (int primeMultiple = 2 * prime; primeMultiple <= primes
-          .get(primes.size() - 1); primeMultiple += prime) {
-        if (primes.contains(primeMultiple)) {
-          primes.remove(primes.indexOf(primeMultiple));
-        }
-      }
-      iteration++;
-    }
-
     while (input.hasNextInt()) {
       int X = input.nextInt();
       int K = 0;
-      iteration = 0;
 
-      while (X > primes.get(iteration)) {
-        if (X % primes.get(iteration) == 0) {
-          X /= primes.get(iteration);
+      int primeFactor = 2;
+
+      while (primeFactor * primeFactor <= X) {
+        while (X % primeFactor == 0) {
           K++;
+          X /= primeFactor;
         }
-        iteration++;
+        primeFactor += primeFactor == 2 ? 1 : 2;
+      }
+
+      if (X > 2) {
+        K++;
       }
 
       System.out.println(K);
